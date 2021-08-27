24dc2a2a8b306bb2ed03dc4036bd94c885f23f83
Deneys S. Maartens <deneys.maartens@gmail.com>
2021-08-27 17:26:27 +0200

    ansi.h: add
    https://gist.github.com/RabaDabaDoba/145049536f815903c79944599c6f952a

---
+++ ansi.h
@@ -0,0 +1,77 @@
+#ifndef ANSI_H
+#define ANSI_H
+
+/* Regular text */
+#define  ANSI_BLK "\033[0;30m"
+#define  ANSI_RED "\033[0;31m"
+#define  ANSI_GRN "\033[0;32m"
+#define  ANSI_YEL "\033[0;33m"
+#define  ANSI_BLU "\033[0;34m"
+#define  ANSI_MAG "\033[0;35m"
+#define  ANSI_CYN "\033[0;36m"
+#define  ANSI_WHT "\033[0;37m"
+
+/* Regular bold text */
+#define  ANSI_BBLK "\033[1;30m"
+#define  ANSI_BRED "\033[1;31m"
+#define  ANSI_BGRN "\033[1;32m"
+#define  ANSI_BYEL "\033[1;33m"
+#define  ANSI_BBLU "\033[1;34m"
+#define  ANSI_BMAG "\033[1;35m"
+#define  ANSI_BCYN "\033[1;36m"
+#define  ANSI_BWHT "\033[1;37m"
+
+/* Regular underline text */
+#define  ANSI_UBLK "\033[4;30m"
+#define  ANSI_URED "\033[4;31m"
+#define  ANSI_UGRN "\033[4;32m"
+#define  ANSI_UYEL "\033[4;33m"
+#define  ANSI_UBLU "\033[4;34m"
+#define  ANSI_UMAG "\033[4;35m"
+#define  ANSI_UCYN "\033[4;36m"
+#define  ANSI_UWHT "\033[4;37m"
+
+/* Regular background */
+#define  ANSI_BLKB "\033[40m"
+#define  ANSI_REDB "\033[41m"
+#define  ANSI_GRNB "\033[42m"
+#define  ANSI_YELB "\033[43m"
+#define  ANSI_BLUB "\033[44m"
+#define  ANSI_MAGB "\033[45m"
+#define  ANSI_CYNB "\033[46m"
+#define  ANSI_WHTB "\033[47m"
+
+/* High intensty background  */
+#define  ANSI_BLKHB "\033[0;100m"
+#define  ANSI_REDHB "\033[0;101m"
+#define  ANSI_GRNHB "\033[0;102m"
+#define  ANSI_YELHB "\033[0;103m"
+#define  ANSI_BLUHB "\033[0;104m"
+#define  ANSI_MAGHB "\033[0;105m"
+#define  ANSI_CYNHB "\033[0;106m"
+#define  ANSI_WHTHB "\033[0;107m"
+
+/* High intensty text */
+#define  ANSI_HBLK "\033[0;90m"
+#define  ANSI_HRED "\033[0;91m"
+#define  ANSI_HGRN "\033[0;92m"
+#define  ANSI_HYEL "\033[0;93m"
+#define  ANSI_HBLU "\033[0;94m"
+#define  ANSI_HMAG "\033[0;95m"
+#define  ANSI_HCYN "\033[0;96m"
+#define  ANSI_HWHT "\033[0;97m"
+
+/* Bold high intensity text */
+#define  ANSI_BHBLK "\033[1;90m"
+#define  ANSI_BHRED "\033[1;91m"
+#define  ANSI_BHGRN "\033[1;92m"
+#define  ANSI_BHYEL "\033[1;93m"
+#define  ANSI_BHBLU "\033[1;94m"
+#define  ANSI_BHMAG "\033[1;95m"
+#define  ANSI_BHCYN "\033[1;96m"
+#define  ANSI_BHWHT "\033[1;97m"
+
+/* Reset */
+#define  ANSI_RESET "\033[0m"
+
+#endif
