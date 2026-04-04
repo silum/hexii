8bd3af01790abc0d0d8de5ed5aa6210ee0eaa65e
Deneys S. Maartens <deneys.maartens@gmail.com>
2026-04-04 16:39:09 +0200

    hexii.c: remove macOS stat() race workaround

--- hexii.c
+++ hexii.c
@@ -190,16 +190,8 @@ fsize(int fd)
 	if (-1 == ret) {
 		return ret;
 	}
-	/* WTF?! -- there appears to be a race condition on macOS, which causes the
-	stat struct to only be partially populated on the first call -- most
-	notably, the `st_size` field has a value of 0! */
-	ret = fstat(fd, &st);
-	if (-1 == ret) {
-		return ret;
-	}
 
-	off_t size = st.st_size;
-	return size;
+	return st.st_size;
 }
 
 static
